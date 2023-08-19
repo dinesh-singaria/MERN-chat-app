317e5133f915c8e3c6fc30a6c08f50cb33438ba8
Dinesh Singaria <dineshsingaria.111@gmail.com>
2023-08-20 04:27:19 +0530

    all done

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,78 +1,17 @@
-// import React, { useEffect } from 'react'
-// import { Box,  Container, Text } from '@chakra-ui/react'
-// import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-// import Login from '../Components/authentication/Login'
-// import Signup from '../Components/authentication/Signup'
-// import { useNavigate } from 'react-router-dom'
-
-// const Homepage = () => {
-//   const navigate = useNavigate();
-    
-//   useEffect(()=>{
-//     const user = JSON.stringify(localStorage.getItem("userInfo"));
-
-
-//     if (user){
-//       navigate("/")
-//     }
-//   },[navigate])
-
-
-//   return (
-//     <Container>
-//       <Box 
-//       d="flex"
-//       justifyContent="Center"
-//       p={3}
-//       bg={"white"}
-//       w="100%"
-//       m="40px 0 15px 0"
-//       borderRadius={"lg"}
-//       borderWidth={"1px"}
-//       >
-//         <Text fontSize="4xl" fontFamily="Work sans" color="black" >Chatties</Text>
-
-//       </Box>
-//       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-// <Tabs variant='soft-rounded' >
-//   <TabList mb={"1em"}>
-//     <Tab width={"50%"}>Login</Tab>
-//     <Tab width={"50%"}>Sign Up</Tab>
-//   </TabList>
-//   <TabPanels>
-//     <TabPanel>
-//       <Login />
-
-//     </TabPanel>
-//     <TabPanel>
-//      <Signup />
-     
-//     </TabPanel>
-//   </TabPanels>
-// </Tabs>
-//       </Box>
-//     </Container>
-//   )
-// }
-
-// export default Homepage
-
-
-
-import {
-  Box,
-  Container,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  Text,
-} from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Login from "../Components/authentication/Login";
 import Signup from "../Components/authentication/Signup";
+import {
+  Box,
+  Container,
+  Tabs,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
 
 function Homepage() {
   const navigate = useNavigate();
@@ -95,7 +34,7 @@ function Homepage() {
         borderRadius="lg"
         borderWidth="1px"
       >
-        <Text textAlign='center' fontSize="4xl" fontFamily="Work sans">
+        <Text textAlign="center" fontSize="4xl" fontFamily="Work sans">
           Messenger
         </Text>
       </Box>

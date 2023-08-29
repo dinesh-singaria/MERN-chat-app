631c8446010178f3f9d9169839c966ed6d53e9b5
Dinesh Singaria <dineshsingaria.111@gmail.com>
2023-08-29 17:53:44 +0530

    mongo db connection

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
-const connectDB = async()=>{
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        // console.log(`Mongo DB connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.log(`Error: ${error.massage}`); 
-        process.exit()
-    }
+    console.log(`Mongo DB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.massage}`);
+    process.exit();
+  }
 };
 
-module.exports= connectDB;
+module.exports = connectDB;

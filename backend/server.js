b5d70606c45266d1c086e6a6dcb0ec1ab071ec83
Dinesh Singaria <dineshsingaria.111@gmail.com>
2023-09-01 20:23:41 +0530

    serverjs updated

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const connectDB = require("./Config/db");
+const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
 
-const { notFound } = require("./middleware/errorMiddleware");
-const { errorHandler } = require("./middleware/errorMiddleware");
-
-const app = express();
 dotenv.config();
 connectDB();
+const app = express();
 
-app.use(express.json());
+app.use(express.json()); // to accept json data
 
-// app.get('/',(req,res)=>{
-//     res.send("homepage");
-// })
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
 
@@ -37,36 +37,38 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 const server = app.listen(
   PORT,
-  console.log(`server started running on port ${PORT}`)
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
+
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://mern-chat-app-nj1p.onrender.com",
+    origin: "http://localhost:3000",
+    // credentials: true,
   },
 });
 
 io.on("connection", (socket) => {
-  // console.log("Connection established");
-
-  socket.on("setup", (user) => {
-    socket.join(user._id);
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
     socket.emit("connected");
-    // console.log(user._id);
   });
 
   socket.on("join chat", (room) => {
     socket.join(room);
-    // console.log("User joined room: " + room);
+    console.log("User Joined Room: " + room);
   });
-
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
